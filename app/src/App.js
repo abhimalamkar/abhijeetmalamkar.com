0e435d23ebd3ef1b0d7881debda02cf5beaebbfe
abhimalamkar <abhijeetmalamkar@gmail.com>
2018-10-11 12:18:46 +0530

    basic Change

--- app/src/App.js
+++ app/src/App.js
@@ -9,15 +9,15 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Abhijeet Malamkar
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://https://github.com/abhimalamkar"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Github
           </a>
         </header>
       </div>
