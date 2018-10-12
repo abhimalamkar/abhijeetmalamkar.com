39b332c176ad1088f7c4f128813e31e43f90dbbb
abhimalamkar <abhijeetmalamkar@gmail.com>
2018-10-12 10:15:18 +0530

    update

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import ThreeScene from "./components/ThreeScene";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <ThreeScene />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Abhijeet Malamkar
-          </p>
+          <p>Abhijeet Malamkar</p>
           <a
             className="App-link"
             href="https://github.com/abhimalamkar"
